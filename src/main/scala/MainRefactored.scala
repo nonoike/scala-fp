3a436b0e37f2e9201d7be0ef287cac957d08cf87
nonoike <ij4nu8d4fw@gmail.com>
2018-07-07 01:41:47 +0900

    add: 04.advanced error chapter

---
+++ src/main/scala/MainRefactored.scala
@@ -0,0 +1,60 @@
+object MainRefactored {
+
+  case class Address(id: Int, name: String, postalCode: Option[String])
+
+  case class User(id: Int, name: String, addressId: Option[Int])
+
+  val userDatabase: Map[Int, User] = Map(
+    1 -> User(1, "太郎", Some(1)),
+    2 -> User(2, "二郎", Some(2)),
+    3 -> User(3, "プー太郎", None)
+  )
+
+  val addressDatabase: Map[Int, Address] = Map(
+    1 -> Address(1, "渋谷", Some("150-0002")),
+    2 -> Address(2, "国際宇宙ステーション", None)
+  )
+
+  sealed abstract class PostalCodeResult
+
+  case class Success(postalCode: String) extends PostalCodeResult
+
+  abstract class Failure extends PostalCodeResult
+
+  case object UserNotFound extends Failure
+
+  case object UserNotHasAddress extends Failure
+
+  case object AddressNotFound extends Failure
+
+  case object AddressNotHasPostalCode extends Failure
+
+  def getPostalCodeResult(userId: Int): PostalCodeResult = {
+    // Either型はmapやflatMapがないため、RightProjection型にして処理をする
+    // EitherがLeftの場合は処理を行わない
+    // .mergeで中身を畳み込んで取得する
+    (for {
+      user <- findUser(userId).right
+      address <- findAddress(user).right
+      postalCode <- findPostalCode(address).right
+    } yield Success(postalCode)).merge
+  }
+
+  def findUser(userId: Int): Either[Failure, User] = userDatabase.get(userId).toRight(UserNotFound)
+
+  def findAddress(user: User): Either[Failure, Address] = {
+    for {
+      addressId <- user.addressId.toRight(UserNotHasAddress).right
+      address <- addressDatabase.get(addressId).toRight(AddressNotFound).right
+    } yield address
+  }
+
+  def findPostalCode(address: Address): Either[Failure, String] = address.postalCode.toRight(AddressNotHasPostalCode)
+
+  def main(args: Array[String]): Unit = {
+    println(getPostalCodeResult(1))
+    println(getPostalCodeResult(2))
+    println(getPostalCodeResult(3))
+    println(getPostalCodeResult(4))
+  }
+}
--- src/main/scala/ShowablePair.scala
+++ src/main/scala/ShowablePair.scala
@@ -6,26 +6,27 @@ class ShowablePair[T1 <: Show, T2 <: Show](val t1: T1, val t2: T2) extends Show 
   override def show: String = s"(${t1.show}, ${t2.show})"
 }
 
-// 型注釈を与えることで抽象的に扱えるようになる
-// search(Seq("a", "b", "c", "d"), { x: String => x == "a" })
-def search[A](seq: Seq[A], f: A => Boolean): Boolean = {
-  def searchRec(i: Int): Boolean = {
-    if (seq.length == i) false
-    else if (f(seq(i))) true
-    else searchRec(i + 1)
-  }
-
-  searchRec(0)
-}
-
-// カリー化することで型注釈が不要になり可読性も向上する
-// search(Seq("a", "b", "c", "d")){ x => x == "a" }
-def search[A](seq: Seq[A])(f: A => Boolean): Boolean = {
-  def searchRec(i: Int): Boolean = {
-    if (seq.length == i) false
-    else if (f(seq(i))) true
-    else searchRec(i + 1)
-  }
-
-  searchRec(0)
-}
+//
+//// 型注釈を与えることで抽象的に扱えるようになる
+//// search(Seq("a", "b", "c", "d"), { x: String => x == "a" })
+//def search[A](seq: Seq[A], f: A => Boolean): Boolean = {
+//  def searchRec(i: Int): Boolean = {
+//    if (seq.length == i) false
+//    else if (f(seq(i))) true
+//    else searchRec(i + 1)
+//  }
+//
+//  searchRec(0)
+//}
+//
+//// カリー化することで型注釈が不要になり可読性も向上する
+//// search(Seq("a", "b", "c", "d")){ x => x == "a" }
+//def search[A](seq: Seq[A])(f: A => Boolean): Boolean = {
+//  def searchRec(i: Int): Boolean = {
+//    if (seq.length == i) false
+//    else if (f(seq(i))) true
+//    else searchRec(i + 1)
+//  }
+//
+//  searchRec(0)
+//}
